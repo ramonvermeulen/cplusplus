1b627ab127311c23e1f7902c670aad9a3c0c608b
Ramon Vermeulen <vermeulen@coffeeit.nl>
2019-11-21 21:28:09 +0100

    walls

--- week1/main.cpp
+++ week1/main.cpp
@@ -9,7 +9,7 @@
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_WIDTH = 640;
 const float WALL_SIZE = 10.0;
-const int AMOUNT_OF_SCREEN_OBJECTS = 5;
+const int AMOUNT_OF_SCREEN_OBJECTS = 6;
 
 int main()
 {
@@ -17,10 +17,15 @@ int main()
     
     Shape* screen_objects[AMOUNT_OF_SCREEN_OBJECTS];
     screen_objects[0] = new Square { sf::Vector2f{ SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 4 * 3) }, 20.0 };
-    screen_objects[1] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Yellow, 15.0, 180, 10 };
-    screen_objects[2] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Blue, 35.0, 225, 15 };
-    screen_objects[3] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Red, 20.0, 70, 20 };
-    screen_objects[4] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Cyan, 10.0, 315, 30 };
+    screen_objects[1] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Yellow, 35.0, 180, 2 };
+    screen_objects[2] = new Wall { sf::Vector2f{ 0.0, 0.0 }, SCREEN_WIDTH, WALL_SIZE, sf::Color::Green };  
+    screen_objects[3] = new Wall { sf::Vector2f{ SCREEN_WIDTH - WALL_SIZE, 0.0 }, WALL_SIZE, SCREEN_HEIGHT, sf::Color::Green };
+    screen_objects[4] = new Wall { sf::Vector2f{ 0.0, SCREEN_HEIGHT - WALL_SIZE }, SCREEN_WIDTH, WALL_SIZE, sf::Color::Green };
+    screen_objects[5] = new Wall { sf::Vector2f{ 0.0, 0.0 }, WALL_SIZE, SCREEN_WIDTH, sf::Color::Green }; 
+
+    // screen_objects[2] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Blue, 35.0, 225, 15 };
+    // screen_objects[3] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Red, 20.0, 70, 20 };
+    // screen_objects[4] = new Ball { sf::Vector2f{ SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, sf::Color::Cyan, 10.0, 315, 30 };
 
     // Wall top_wall { sf::Vector2f{ 0.0, 0.0 }, SCREEN_WIDTH, WALL_SIZE, sf::Color::Green };    
     // Wall right_wall { sf::Vector2f{ SCREEN_WIDTH - WALL_SIZE, 0.0 }, WALL_SIZE, SCREEN_HEIGHT, sf::Color::Green };
